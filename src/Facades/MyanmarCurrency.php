2d3195498f357e947c820a7b21379039e1bcdbf5
myathtutthu-genie <myathtutthu@geniefintech.com>
2023-02-25 12:16:34 +0630

    feat: upgrade laravel 10 and PHP.

--- src/Facades/MyanmarCurrency.php
+++ src/Facades/MyanmarCurrency.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \MyanmarCurrency\MyanmarCurrency\MyanmarCurrency
+ *
  * * @method static string convertMyanmarText(int $number)
  */
 class MyanmarCurrency extends Facade
--- src/Traits/ValidationTraitException.php
+++ src/Traits/ValidationTraitException.php
@@ -8,16 +8,16 @@ trait ValidationTraitException
 {
     public function checkValidationException($number)
     {
-        $number_validation_regex = "/^(?:-(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))|(?:0|(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))))(?:.\\d+|)$/";
+        $number_validation_regex = '/^(?:-(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))|(?:0|(?:[1-9](?:\\d{0,2}(?:,\\d{3})+|\\d*))))(?:.\\d+|)$/';
         if (preg_replace($number_validation_regex, '', $number)) {
-            throw new InvalidArgumentException("Please type number only!");
+            throw new InvalidArgumentException('Please type number only!');
         }
 
-        if (preg_match("/[^0-9]/", $number)) {
-            throw new InvalidArgumentException("Please type number only!");
+        if (preg_match('/[^0-9]/', $number)) {
+            throw new InvalidArgumentException('Please type number only!');
         }
         if (preg_match("/^(?:0|00)\d+$/", $number)) {
-            throw new InvalidArgumentException("Number should not start with 0");
+            throw new InvalidArgumentException('Number should not start with 0');
         }
     }
 }
